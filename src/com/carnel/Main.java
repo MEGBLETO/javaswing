5615f07e9a047192b70a8d193c709e1347016e04
carnel MEGBLETO <carnelmegbleto@carnels-macbook-pro.home>
2022-01-03 13:56:58 +0100

    first commit

---
+++ src/com/carnel/Main.java
@@ -0,0 +1,17 @@
+package com.carnel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        frame.setTitle("Carnel is learning");
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.black);
+    }
+}
